8e630d34cd35647f1de35d92e27155f91581581e
Lennaert van der Linden <lennaertvanderlinden@gmail.com>
2017-10-23 22:40:26 +0200

    refactor: replaced custom number format function with Number.toLocaleString

--- src/filters/price.js
+++ src/filters/price.js
@@ -1,20 +1,6 @@
-export default value => {
-  let whole = Math.floor(value)
-  let fraction = Math.round((value - whole) * 100)
-
-  if (fraction === 100) {
-    fraction = 0;
-    whole += 1
-  }
-
-  // thousands separator
-  whole = '' + whole
-  while(whole.match(/^\d{4}/) !== null) {
-      whole = whole.replace(/(\d)(\d{3})(\,|$)/, '$1,$2$3')
-  }
-
-  // padding for fraction
-  fraction = (fraction < 10 ? '0' : '') + fraction
-
-  return '$ ' + whole + '.' + fraction;
-}
+export default value => Number(value).toLocaleString('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+})
